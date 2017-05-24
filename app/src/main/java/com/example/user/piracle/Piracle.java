2f79afaf0bcc5e73138d243b09ec36da8c6cdb3a
pipi9baby <pipi9baby@gmail.com>
2017-05-24 22:52:03 +0800

    error改掉了

--- app/src/main/java/com/example/user/piracle/Piracle.java
+++ app/src/main/java/com/example/user/piracle/Piracle.java
@@ -80,17 +80,15 @@ public class Piracle extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_setting) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_open) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_recently) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_cloud) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_help) {
 
         }
 
